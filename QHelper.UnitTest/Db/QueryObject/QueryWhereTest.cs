ef832f9b983ba2631f8b29e05b5844b42d0becad
chinaq <aaa6263@sina.com>
2015-04-03 12:50:51 +0800

    create repo

---
+++ QHelper.UnitTest/Db/QueryObject/QueryWhereTest.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QHelper.UnitTest.Db.QueryObject
+{
+    [TestClass]
+    public class QueryWhereTest
+    {
+        [TestMethod]
+        public void Test_GetClause() { 
+            //Arrange
+            WhereQuery query = new WhereQueryCom();
+            WhereCriteria cri1 = new WhereCriteria();
+            WhereCriteria cri2 = new WhereCriteria();
+
+            query.Add(cri1);
+            query.Add(cri2);
+
+            //Action
+            WhereClause clause = query.GeneralClause();
+
+            //Assert
+            Assert.AreEqual("Name = @0 AND Point = @1", clause.sqlStr);
+        }
+    }
+}
